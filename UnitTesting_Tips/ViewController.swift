1267f3dd5a3ddfdaff16073b31c58938652cee01
abuzeid <abozaid.hamed@vodafone.com>
2018-12-21 20:22:58 +0200

    Initial Commit

---
+++ UnitTesting_Tips/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UnitTesting_Tips
+//
+//  Created by Abuzeid Ibrahim on 12/21/18.
+//  Copyright © 2018 abuzeid. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ UnitTesting_TipsTests/UnitTesting_TipsTests.swift
@@ -0,0 +1,34 @@
+//
+//  UnitTesting_TipsTests.swift
+//  UnitTesting_TipsTests
+//
+//  Created by Abuzeid Ibrahim on 12/21/18.
+//  Copyright © 2018 abuzeid. All rights reserved.
+//
+
+import XCTest
+@testable import UnitTesting_Tips
+
+class UnitTesting_TipsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
